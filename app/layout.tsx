3329b76db490599dda63077f47a9cf3eb9361618
SEBASTIAN MORENO <seba202811@uc.cl>
2025-12-29 00:55:18 -0300

    verificacion google 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           </BetProvider>
         </ProductsProvider> 
       </body>
-      <meta name="google-site-verification" content="USmfcC17Kj1WRruEUjUyDjT6Mw-nlQuWykjmbshN7XY" />
+      <meta name="google-site-verification" content="RGA8T8bZSUjuiwnKbeDj72PjJtxeZINh2Wk1UGJBt8Y" />
     </html>
   )
 }
